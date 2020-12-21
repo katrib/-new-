e0337ae41ffef87fcb143ee90cb35af75b8e2cc5
Katrib_8 <ekaterina12345qwas@gmail.com>
2020-12-21 21:57:04 +0700

    the first

---
+++ course_work/course_work/AddDialog.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace course_work
+{
+    /// <summary>
+    /// Логика взаимодействия для AddDialog.xaml
+    /// </summary>
+    public partial class AddDialog : Window
+    {
+        public AddDialog()
+        {
+            InitializeComponent();
+        }
+
+        //происходит назначение имени, выбор пути к файлу и определение его в категорию 
+        private void FindWay_Click(object sender, RoutedEventArgs e)
+        {
+            //выбор медиа файла, например, в формате .mp3
+            OpenFileDialog dlg = new OpenFileDialog();
+            DialogResult dlgres = dlg.ShowDialog();
+
+            if (dlgres == System.Windows.Forms.DialogResult.OK)
+            {
+                string FileName = dlg.FileName;
+                fileName.Text = FileName;
+            }
+        }
+
+        private void Ok_Click_1(object sender, RoutedEventArgs e)
+        {
+           
+            if (fn == null)
+                System.Windows.MessageBox.Show("У вас не задано имя файла");
+            else
+                this.DialogResult = true;
+        }
+
+        private void addTags_Click(object sender, RoutedEventArgs e)
+        {
+            listtags.Items.Add(ftags.Text);
+        }
+
+        //окно создания , окно добавления (наим.)
+    }
+}
---
+++ course_work/course_work/JSON.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace course_work
+{
+    public class JSON
+    {
+        string FileName;
+        public void SaveFile(List<imgOBJECT> imgs)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            DialogResult dlgres = dlg.ShowDialog();
+            JsonSerializer serializer = new JsonSerializer();
+
+            if (dlgres == System.Windows.Forms.DialogResult.OK)
+            {
+              
+                
+                using (StreamWriter sw = new StreamWriter(dlg.FileName))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, imgs);
+                }
+            }
+
+        }
+        public List<imgOBJECT> LoadFile()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            DialogResult dlgres = dlg.ShowDialog(); 
+
+            if (dlgres == System.Windows.Forms.DialogResult.OK)
+            {
+                FileName = dlg.FileName;
+            }
+            return JsonConvert.DeserializeObject<List<imgOBJECT>>
+                (File.ReadAllText(FileName));
+     
+        }
+    }
+
+}
